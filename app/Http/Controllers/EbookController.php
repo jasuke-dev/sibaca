d6e9c14d3383e60a2417260891365fbd42eea205
jasuke-dev <ilham.suryaega@gmail.com>
2022-02-18 10:53:12 +0700

    basic fitur menambah collection

--- app/Http/Controllers/EbookController.php
+++ app/Http/Controllers/EbookController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Ebook;
 use App\Http\Requests\StoreEbookRequest;
 use App\Http\Requests\UpdateEbookRequest;
+use App\Models\Author;
+use App\Models\Language;
+use App\Models\Type;
+use Illuminate\Http\Request;
 
 class EbookController extends Controller
 {
@@ -28,7 +32,13 @@ class EbookController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.pages.lists.create',[
+            'title' => 'Collections',
+            'page' => 'collections',
+            'types' => Type::all(),
+            'languages' => Language::all(),
+            'authors' => Author::all(),
+        ]);
     }
 
     /**
@@ -37,9 +47,40 @@ class EbookController extends Controller
      * @param  \App\Http\Requests\StoreEbookRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreEbookRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:255',
+            'isbn_issn_doi' => 'required|max:255',
+            'abstract' => 'required|max:255',
+            'publish_year' => 'required|max:255',
+            'type' => 'required|max:255',
+            'language' => 'required|max:255',
+            'author' => 'required|max:255',
+            'file' => 'required|mimes:pdf|max:5048',
+            'cover' => ['required','mimes:png,jpg,jpeg','max:5048'],
+        ]);
+
+        $path = $request->file('file')->store('public/files/ebooks');
+        $path_cover = $request->file('cover')->store('public/files/covers');
+
+        $validated = [
+          'title' => $request['title'],
+          'isbn_issn_doi' => $request['isbn_issn_doi'],
+          'abstract' => $request['abstract'],
+          'publish_year' => $request['publish_year'],
+          'type' => $request['type'],
+          'language' => $request['language'],
+          'author' => $request['author'],
+          'path_file' => $path,
+          'path_cover' => $path_cover,
+        ];        
+        try {
+            Ebook::create($validated);
+            return redirect('/admin/collections')->with('success',"New Collections has been aded!");
+        } catch (\Exception $e) {
+            return redirect('/admin/collections')->with('error',$e->getMessage());
+        }
     }
 
     /**
--- resources/views/admin/pages/lists/create.blade.php
+++ resources/views/admin/pages/lists/create.blade.php
@@ -15,22 +15,22 @@
         </div>
 
         {{-- form --}}
-        <form action="/admin/{{ $page }}" method="POST">
+        <form action="/admin/{{ $page }}" method="POST" enctype="multipart/form-data">
             @csrf
-            <div class="mx-8 px-8 py-4 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
-                <label for="{{ $page == 'users' ? 'username' : $page }}" class="block mt-4 text-sm">
-                    <span class="text-gray-700 dark:text-gray-400">{{ $title }}</span>
-                    <input type="text" class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" placeholder="Fill New {{ $title }}" name="{{ $page == 'users' ? 'username' : $page }}" required autofocus>
-                    @error("{{ $page == 'users' ? 'username' : $page }}")
-                        <div class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </label>
-                @if ($page == 'users')    
+            <div class="w-8/12 mx-8 px-8 py-4 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+                @if ($page == 'users')
+                    <label for="username" class="block mt-4 text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">{{ $title }}</span>
+                        <input type="text" class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" placeholder="Fill New {{ $title }}" name="username" required autofocus>
+                        @error("username")
+                            <div class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </label>    
                     <label for="type" class="block mt-4 text-sm">
                         <span class="text-gray-700 dark:text-gray-400">Password</span>
-                        <input type="text" class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" placeholder="Fill Password" name="password" required autofocus>
+                        <input type="text" class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" placeholder="Fill Password" name="password" required>
                         @error('type')
                             <div class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
                                 {{ $message }}
@@ -66,6 +66,123 @@
                           </label>
                         </div>
                       </div>
+
+                @elseif ($page == 'collections')    
+                    <label for="title" class="block mt-4 text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">Title</span>
+                        <input type="text" class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" placeholder="New title collection" name="title" value="{{ old('title') }}" required autofocus>
+                        @error("title")
+                            <div class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </label>  
+                    <label for="isbn_issn_doi" class="block mt-4 text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">ISBN/ISSN/DOI</span>
+                        <input type="text" class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" placeholder="ISBN/ISSN/DOI" value="{{ old('isbn_issn_doi') }}" name="isbn_issn_doi" required>
+                        @error('isbn_issn_doi')
+                            <div class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </label>
+                    <label for="abstract" class="block mt-4 text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">Abstract</span>
+                        <input type="text" class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" placeholder="Abstract" value="{{ old('abstract') }}" name="abstract" required>
+                        @error('abstract')
+                            <div class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </label>
+                    <label for="publish_year" class="block mt-4 text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">Publication year</span>
+                        <input type="text" id="publish_year" class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" placeholder="Publication Year" value="{{ old('publish_year') }}" name="publish_year" required>
+                        @error('publish_year')
+                            <div class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </label>
+                    <label for="type" class="block mt-4 text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">Type Collection</span>
+                        <select class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" name="type" required>
+                          @foreach ($types as $type)
+                            @if (old('type') == $type->id)
+                              <option value="{{ $type->id }}" selected>{{ $type->type }}</option>
+                            @else
+                              <option value="{{ $type->id }}">{{ $type->type }}</option>
+                            @endif
+                          @endforeach
+                        </select>
+                        @error('type')
+                        <div class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                          {{ $message }}
+                        </div>
+                        @enderror
+                    </label>
+                    <label for="language" class="block mt-4 text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">Language</span>
+                        <select class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" value="{{ old('language') }}" name="language" required>
+                          @foreach ($languages as $language)
+                            @if (old('type') == $language->id)
+                              <option value="{{ $language->id }}" selected>{{ $language->language }}</option>
+                            @else
+                              <option value="{{ $language->id }}">{{ $language->language }}</option>
+                            @endif
+                          @endforeach
+                        </select>
+                        @error('language')
+                            <div class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </label>
+                    <label for="author" class="block mt-4 text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">Author</span>
+                        <select class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" value="{{ old('author') }}" name="author" required>
+                          @foreach ($authors as $author)
+                            @if (old('type') == $author->id)
+                              <option value="{{ $author->id }}" selected>{{ $author->author }}</option>
+                            @else
+                              <option value="{{ $author->id }}">{{ $author->author }}</option>
+                            @endif
+                          @endforeach
+                        </select>
+                        @error('author')
+                            <div class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </label>
+                    <label for="file" class="block mt-4 text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">File</span>
+                        <input type="file" class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" placeholder="Ebook File" name="file">
+                        @error('file')
+                            <div class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </label>
+                    <label for="Cover" class="block mt-4 text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">Cover</span>
+                        <input type="file" class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" placeholder="Cover Image" name="cover">
+                        @error('cover')
+                            <div class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </label>
+                @else
+                    <label for="{{ $page == 'users' ? 'username' : $page }}" class="block mt-4 text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">{{ $title }}</span>
+                        <input type="text" class="block w-full text-sm bg-gray-100 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md p-3 mt-3 appearance-none border" placeholder="Fill New {{ $title }}" name="{{ $page == 'users' ? 'username' : $page }}" required autofocus>
+                        @error("{{ $page == 'users' ? 'username' : $page }}")
+                            <div class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </label>
                 @endif
 
                 <button class="flex items-center justify-between mt-6 px-8 py-4 font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700" type="submit">
