c7f11a8c3ee42f7ee864a1cf1251236c2f2c8a55
jasuke-dev <ilham.suryaega@gmail.com>
2022-03-31 09:07:20 +0700

    menginstal plugin alpine presist guna menyimpan data query builder ketika reload

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 // require('./bootstrap');
 import Alpine from 'alpinejs'
+import persist from '@alpinejs/persist'
 window.Alpine = Alpine
 
 window.TomSelect = require('tom-select');
 
+Alpine.plugin(persist)
 Alpine.start()
